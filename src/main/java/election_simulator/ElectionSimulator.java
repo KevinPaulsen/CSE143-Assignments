11f2c6b0eae9f28af7ee7900bd6508b86d369e0f
Kevin Paulsen <kevinpaulsen@MacBook-Pro.local>
2020-11-11 23:42:39 -0800

    Updated ElectionSimulator

--- src/main/java/election_simulator/ElectionSimulator.java
+++ src/main/java/election_simulator/ElectionSimulator.java
@@ -14,7 +14,7 @@ import java.io.*;
 public class ElectionSimulator {
 
     private final List<State> states;
-    private final Map<Arguments, Set<State>> combinations;
+    private final Map<Arguments, Node> combinations;
 
     ElectionSimulator(List<State> states) {
         this.states = states;
@@ -22,36 +22,42 @@ public class ElectionSimulator {
     }
 
     Set<State> simulate() {
-        Set<State> test = simulate(minElectoralVotes(states), 0);
-        System.out.println(combinations.size());
-        return test;
+        Node node = simulate(minElectoralVotes(states), 0);
+        if (node != null) {
+            return node.toHashSet();
+        }
+        return null;
     }
 
-    private Set<State> simulate(int electoralVotesRemaining, int stateIndex) {
+    private Node simulate(int electoralVotesRemaining, int stateIndex) {
         Arguments arguments = new Arguments(electoralVotesRemaining, stateIndex);
         if (combinations.containsKey(arguments)) {
             return combinations.get(arguments);
         } else if (electoralVotesRemaining <= 0) {
-            return new HashSet<>();
+            return new Node(null, null);
         } else if (stateIndex == states.size()) {
             combinations.put(arguments, null);
             return null;
         }
 
         int minVotes = Integer.MAX_VALUE;
-        Set<State> result = null;
+        Node result = null;
 
         for (int currentIdx = stateIndex; currentIdx < states.size(); currentIdx++) {
             State currentState = states.get(currentIdx);
-            Set<State> minRemainingStates = simulate(
+            Node minRemainingStates = simulate(
                     electoralVotesRemaining - currentState.electoralVotes,
                     currentIdx + 1);
             if (minRemainingStates != null) {
-                minRemainingStates.add(currentState);
+                if (minRemainingStates.state == null) {
+                    minRemainingStates.state = currentState;
+                } else {
+                    minRemainingStates = new Node(currentState, minRemainingStates);
+                }
                 int numVotes = minPopularVotes(minRemainingStates);
                 if (numVotes < minVotes) {
                     minVotes = numVotes;
-                    result = new HashSet<>(minRemainingStates);
+                    result = minRemainingStates;
                 }
             }
         }
@@ -67,7 +73,16 @@ public class ElectionSimulator {
         return total / 2 + 1;
     }
 
-    public static int minPopularVotes(Set<State> states) {
+    public static int minPopularVotes(Node node) {
+        int total = 0;
+        while (node != null) {
+            total += node.state.popularVotes / 2 + 1;
+            node = node.next;
+        }
+        return total;
+    }
+
+    public static int minPopularVotes(Collection<State> states) {
         int total = 0;
         for (State state : states) {
             total += state.popularVotes / 2 + 1;
@@ -123,18 +138,46 @@ public class ElectionSimulator {
         }
     }
 
+    private static class Node {
+        private State state;
+        private Node next;
+
+        private Node(State state, Node next) {
+            this.state = state;
+            this.next = next;
+        }
+
+        private Set<State> toHashSet() {
+            Set<State> result = new HashSet<>();
+            Node current = this;
+            while (current != null) {
+                result.add(current.state);
+                current = current.next;
+            }
+            return result;
+        }
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         List<State> states = new ArrayList<>(51);
-        try (Scanner input = new Scanner(new File("/Users/kevinpaulsen/dev/cse143_projects/src/main/java/election_simulator/data/1828.csv"))) {
+        try (Scanner input = new Scanner(new File("/Users/kevinpaulsen/dev/cse143_projects/src/main/java/election_simulator/data/2016.csv"))) {
             while (input.hasNextLine()) {
                 states.add(State.fromCsv(input.nextLine()));
             }
         }
+        new ElectionSimulator(states).simulate();
+        new ElectionSimulator(states).simulate();
+
+        long startTime = System.nanoTime();
         Set<State> result = new ElectionSimulator(states).simulate();
-        System.out.println(result);
+        long endTime = System.nanoTime();
+        System.out.println(((double) endTime - startTime) * 0.000001 + " milliseconds\n\n");
+
+
+        /*System.out.println(result);
         System.out.println(minPopularVotes(result) + " votes");
         System.out.println(minElectoralVotes(states) + " min electoral votes");
         System.out.println(totalElectoralVotes(result) + " electoral votes");
-        System.out.println(((double) minPopularVotes(result)) / totalVotes(states) * 100 + "%");
+        System.out.println(((double) minPopularVotes(result)) / totalVotes(states) * 100 + "%");//*/
     }
 }
